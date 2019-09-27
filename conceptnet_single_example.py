9f1126c6c5fb9369cf15d99c5e946ab5aff96e69
hongmingzhang <hzhangal0330@gmail.com>
2019-09-27 21:08:53 +0800

    generate prediction

--- conceptnet_single_example.py
+++ conceptnet_single_example.py
@@ -3,6 +3,10 @@ import sys
 import argparse
 import torch
 import copy
+import sys
+import os
+sys.path.append( os.path.join( os.path.dirname(__file__), os.path.pardir ) )
+
 
 sys.path.append(os.getcwd())
 
